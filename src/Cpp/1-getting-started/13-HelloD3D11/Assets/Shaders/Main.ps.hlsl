0f10f3f73ab8145c3300455e672bbbbdf28c3c88
AlexB8675 <alexandr.benbaccar@gmail.com>
2022-02-22 22:15:06 +0100

    Hello Triangle with raw D3D11

---
+++ src/Cpp/1-getting-started/13-HelloD3D11/Assets/Shaders/Main.ps.hlsl
@@ -0,0 +1,7 @@
+struct vs_out {
+    float4 clip: SV_POSITION;
+};
+
+float4 main(vs_out input): SV_TARGET{
+    return float4(1.0, 1.0, 1.0, 1.0);
+}
---
+++ src/Cpp/1-getting-started/13-HelloD3D11/Assets/Shaders/Main.vs.hlsl
@@ -0,0 +1,13 @@
+struct vs_in {
+    float3 local: POSITION;
+};
+
+struct vs_out {
+    float4 clip: SV_POSITION;
+};
+
+vs_out main(vs_in input) {
+    vs_out output = (vs_out)0;
+    output.clip = float4(input.local, 1.0);
+    return output;
+}
