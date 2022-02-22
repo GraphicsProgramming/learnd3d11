9ae13f1f037fd78ca3ffcccd454c3037607b8ca4
AlexB8675 <alexandr.benbaccar@gmail.com>
2022-02-22 21:27:47 +0100

    Hello Triangle, raw D3D11

---
+++ src/Cpp/1-getting-started/13-HelloD3D11/Application.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string_view>
+#include <cstdint>
+
+struct GLFWwindow;
+class Application
+{
+public:
+    Application(const std::string_view title);
+    virtual ~Application();
+
+    void Run();
+
+protected:
+    virtual bool Initialize();
+    virtual void Cleanup();
+    virtual void Render() = 0;
+    virtual void Update() = 0;
+
+    [[nodiscard]] GLFWwindow* GetWindow()       const;
+    [[nodiscard]] int32_t     GetWindowWidth()  const;
+    [[nodiscard]] int32_t     GetWindowHeight() const;
+                  void        Close();
+
+private:
+    GLFWwindow*      _window = nullptr;
+    int32_t          _width  = 0;
+    int32_t          _height = 0;
+    std::string_view _title;
+};
