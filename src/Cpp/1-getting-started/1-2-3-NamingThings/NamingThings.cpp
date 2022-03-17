8dcf11ba0662eb78da14765c03ec5a0349957973
deccer <decccer@outlook.com>
2022-03-18 00:18:43 +0100

    Introduce ShaderFactory to NamingThings

--- src/Cpp/1-getting-started/1-2-3-NamingThings/NamingThings.cpp
+++ src/Cpp/1-getting-started/1-2-3-NamingThings/NamingThings.cpp
@@ -131,14 +131,16 @@ bool NamingThingsApplication::Initialize()
 
     CreateSwapchainResources();
 
+    _shaderFactory = std::make_unique<ShaderFactory>(_device);
+
     WRL::ComPtr<ID3DBlob> vertexShaderBlob = nullptr;
-    _vertexShader = CreateVertexShader(L"Assets/Shaders/Main.vs.hlsl", vertexShaderBlob);
+    _vertexShader = _shaderFactory->CreateVertexShader(L"Assets/Shaders/Main.vs.hlsl", vertexShaderBlob);
     if (_vertexShader == nullptr)
     {
         return false;
     }
 
-    _pixelShader = CreatePixelShader(L"Assets/Shaders/Main.ps.hlsl");
+    _pixelShader = _shaderFactory->CreatePixelShader(L"Assets/Shaders/Main.ps.hlsl");
     if (_pixelShader == nullptr)
     {
         return false;
@@ -199,89 +201,6 @@ bool NamingThingsApplication::Initialize()
 
     return true;
 }
-
-bool NamingThingsApplication::CompileShader(
-    const std::wstring_view fileName,
-    const std::string_view entryPoint,
-    const std::string_view profile,
-    WRL::ComPtr<ID3DBlob>& shaderBlob) const
-{
-    UINT compileFlags = D3DCOMPILE_ENABLE_STRICTNESS;
-#if !defined(NDEBUG)
-    compileFlags |= D3DCOMPILE_DEBUG;
-#endif
-
-    WRL::ComPtr<ID3DBlob> tempShaderBlob = nullptr;
-    WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
-    if (FAILED(D3DCompileFromFile(
-        fileName.data(),
-        nullptr,
-        D3D_COMPILE_STANDARD_FILE_INCLUDE,
-        entryPoint.data(),
-        profile.data(),
-        compileFlags,
-        0,
-        &tempShaderBlob,
-        &errorBlob)))
-    {
-        std::cout << "D3D11: Failed to read shader from file\n";
-        if (errorBlob != nullptr)
-        {
-            std::cout << "D3D11: With message: " << static_cast<const char*>(errorBlob->GetBufferPointer()) << "\n";
-        }
-
-        return false;
-    }
-
-    shaderBlob = std::move(tempShaderBlob);
-    return true;
-}
-
-WRL::ComPtr<ID3D11VertexShader> NamingThingsApplication::CreateVertexShader(
-    const std::wstring_view fileName,
-    WRL::ComPtr<ID3DBlob>& vertexShaderBlob) const
-{
-    if (!CompileShader(fileName, "Main", "vs_5_0", vertexShaderBlob))
-    {
-        return nullptr;
-    }
-
-    WRL::ComPtr<ID3D11VertexShader> vertexShader;
-    if (FAILED(_device->CreateVertexShader(
-        vertexShaderBlob->GetBufferPointer(),
-        vertexShaderBlob->GetBufferSize(),
-        nullptr,
-        &vertexShader)))
-    {
-        std::cout << "D3D11: Failed to compile vertex shader\n";
-        return nullptr;
-    }
-
-    return vertexShader;
-}
-
-WRL::ComPtr<ID3D11PixelShader> NamingThingsApplication::CreatePixelShader(const std::wstring_view fileName) const
-{
-    WRL::ComPtr<ID3DBlob> pixelShaderBlob = nullptr;
-    if (!CompileShader(fileName, "Main", "ps_5_0", pixelShaderBlob))
-    {
-        return nullptr;
-    }
-
-    WRL::ComPtr<ID3D11PixelShader> pixelShader;
-    if (FAILED(_device->CreatePixelShader(
-        pixelShaderBlob->GetBufferPointer(),
-        pixelShaderBlob->GetBufferSize(),
-        nullptr,
-        &pixelShader)))
-    {
-        std::cout << "D3D11: Failed to compile pixel shader\n";
-        return nullptr;
-    }
-
-    return pixelShader;
-}
-
 
 bool NamingThingsApplication::CreateSwapchainResources()
 {
