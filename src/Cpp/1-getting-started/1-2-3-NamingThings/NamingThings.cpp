196cd9aa0fa7d1e1c1088815b0cbab304ed26e1a
deccer <decccer@outlook.com>
2022-03-20 14:08:33 +0100

    Migrate NamingThings to Pipeline & friends

--- src/Cpp/1-getting-started/1-2-3-NamingThings/NamingThings.cpp
+++ src/Cpp/1-getting-started/1-2-3-NamingThings/NamingThings.cpp
@@ -1,4 +1,8 @@
 #include "NamingThings.hpp"
+#include "DeviceContext.hpp"
+#include "Pipeline.hpp"
+#include "PipelineFactory.hpp"
+#include "VertexType.hpp"
 
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -22,15 +26,6 @@ inline void SetDebugName(_In_ ID3D11DeviceChild* deviceResource, _In_z_ const ch
     deviceResource->SetPrivateData(WKPDID_D3DDebugObjectName, TDebugNameLength - 1, debugName);
 }
 
-using Position = DirectX::XMFLOAT3;
-using Color = DirectX::XMFLOAT3;
-
-struct VertexPositionColor
-{
-    Position position;
-    Color color;
-};
-
 NamingThingsApplication::NamingThingsApplication(const std::string_view title)
     : Application(title)
 {
@@ -39,14 +34,11 @@ NamingThingsApplication::NamingThingsApplication(const std::string_view title)
 NamingThingsApplication::~NamingThingsApplication()
 {
     _deviceContext->Flush();
-    _pixelShader.Reset();
-    _vertexShader.Reset();
-    _vertexLayout.Reset();
     _triangleVertices.Reset();
     DestroySwapchainResources();
     _swapChain.Reset();
     _dxgiFactory.Reset();
-    _deviceContext.Reset();
+    _deviceContext.reset();
 #if !defined(NDEBUG)
     _debug->ReportLiveDeviceObjects(D3D11_RLDO_FLAGS::D3D11_RLDO_DETAIL);
     _debug.Reset();
@@ -75,6 +67,7 @@ bool NamingThingsApplication::Initialize()
 #if !defined(NDEBUG)
     deviceFlags |= D3D11_CREATE_DEVICE_FLAG::D3D11_CREATE_DEVICE_DEBUG;
 #endif
+    WRL::ComPtr<ID3D11DeviceContext> deviceContext;
     if (FAILED(D3D11CreateDevice(
         nullptr,
         D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_HARDWARE,
@@ -85,21 +78,23 @@ bool NamingThingsApplication::Initialize()
         D3D11_SDK_VERSION,
         &_device,
         nullptr,
-        &_deviceContext)))
+        &deviceContext)))
     {
         std::cout << "D3D11: Failed to create Device and Device Context\n";
         return false;
     }
+
+    constexpr char deviceName[] = "DEV_Main";
+    _device->SetPrivateData(WKPDID_D3DDebugObjectName, sizeof(deviceName), deviceName);
+    SetDebugName(deviceContext.Get(), "CTX_Main");
+
+    _deviceContext = std::make_unique<DeviceContext>(std::move(deviceContext));
 
     if (FAILED(_device.As(&_debug)))
     {
         std::cout << "D3D11: Failed to get the debug layer from the device\n";
         return false;
     }
-
-    constexpr char deviceName[] = "DEV_Main";
-    _device->SetPrivateData(WKPDID_D3DDebugObjectName, sizeof(deviceName), deviceName);
-    SetDebugName(_deviceContext.Get(), "CTX_Main");
 
     DXGI_SWAP_CHAIN_DESC1 swapChainDescriptor = {};
     swapChainDescriptor.Width = GetWindowWidth();
@@ -129,52 +124,12 @@ bool NamingThingsApplication::Initialize()
 
     CreateSwapchainResources();
 
-    _shaderFactory = std::make_unique<ShaderFactory>(_device);
-
-    WRL::ComPtr<ID3DBlob> vertexShaderBlob = nullptr;
-    _vertexShader = _shaderFactory->CreateVertexShader(L"Assets/Shaders/Main.vs.hlsl", vertexShaderBlob);
-    if (_vertexShader == nullptr)
-    {
-        return false;
-    }
-
-    _pixelShader = _shaderFactory->CreatePixelShader(L"Assets/Shaders/Main.ps.hlsl");
-    if (_pixelShader == nullptr)
-    {
-        return false;
-    }
-
-    // ReSharper disable once CppTooWideScopeInitStatement
-    constexpr D3D11_INPUT_ELEMENT_DESC vertexInputLayoutInfo[] =
-    {
-        {
-            "POSITION",
-            0,
-            DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT,
-            0,
-            offsetof(VertexPositionColor, position),
-            D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0
-        },
-        {
-            "COLOR",
-            0,
-            DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT,
-            0,
-            offsetof(VertexPositionColor, color),
-            D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA,
-            0
-        },
-    };
-    if (FAILED(_device->CreateInputLayout(
-        vertexInputLayoutInfo,
-        _countof(vertexInputLayoutInfo),
-        vertexShaderBlob->GetBufferPointer(),
-        vertexShaderBlob->GetBufferSize(),
-        &_vertexLayout)))
-    {
-        std::cout << "D3D11: Failed to create default vertex input layout\n";
-        return false;
-    }
+    _pipelineFactory = std::make_unique<PipelineFactory>(_device);
+    PipelineSettings pipelineSettings = {};
+    pipelineSettings.VertexFilePath = L"Assets/Shaders/Main.vs.hlsl";
+    pipelineSettings.PixelFilePath = L"Assets/Shaders/Main.ps.hlsl";
+    pipelineSettings.VertexType = VertexType::PositionColor;
+    _pipelineFactory->CreatePipeline(pipelineSettings, _pipeline);
 
     constexpr VertexPositionColor vertices[] =
     {
@@ -266,35 +221,12 @@ void NamingThingsApplication::Render()
     viewport.MaxDepth = 1.0f;
 
     constexpr float clearColor[] = { 0.1f, 0.1f, 0.1f, 1.0f };
-    constexpr UINT vertexStride = sizeof(VertexPositionColor);
     constexpr UINT vertexOffset = 0;
 
-    _deviceContext->ClearRenderTargetView(
-        _renderTarget.Get(),
-        clearColor);
-    _deviceContext->IASetInputLayout(_vertexLayout.Get());
-    _deviceContext->IASetVertexBuffers(
-        0,
-        1,
-        _triangleVertices.GetAddressOf(),
-        &vertexStride,
-        &vertexOffset);
-    _deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    _deviceContext->VSSetShader(
-        _vertexShader.Get(),
-        nullptr,
-        0);
-    _deviceContext->RSSetViewports(
-        1,
-        &viewport);
-    _deviceContext->PSSetShader(
-        _pixelShader.Get(),
-        nullptr,
-        0);
-    _deviceContext->OMSetRenderTargets(
-        1,
-        _renderTarget.GetAddressOf(),
-        nullptr);
-    _deviceContext->Draw(3, 0);
+    _deviceContext->Clear(_renderTarget.Get(), clearColor);
+    _deviceContext->SetPipeline(_pipeline.get());
+    _deviceContext->SetVertexBuffer(_triangleVertices.Get(), vertexOffset);
+    _deviceContext->SetViewport(viewport);
+    _deviceContext->Draw();
     _swapChain->Present(1, 0);
 }
